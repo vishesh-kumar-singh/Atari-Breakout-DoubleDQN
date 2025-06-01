d5914f77530e76096e1c2d0e4ef5ce570bf1da9c
vishesh-kumar-singh <thevishesh16@gmail.com>
2025-06-01 19:18:50 +0530

    Main Execution Block

--- Main.py
+++ Main.py
@@ -1,7 +1,20 @@
+from training_config_and_function import train_agent
+from Visualisation_and_plotting import plot_training_results
+
+
 if __name__ == "__main__":
     """Main execution block for the assignment"""
     # Display assignment title and information.
+    print("Assignment: Using Double Q-Learning along with PER to play Atari Breakout")
+    print("Team: Runtime Terrors")
+
     # Execute the training process and collect results.
-    # Generate and display comprehensive visualizations.
+    episode_rewards, mean_scores, losses = train_agent()
+
+    # Generate and display comprehensive visualizations
+    plot_training_results(episode_rewards, mean_scores, losses)
+
     # Print completion message and file references.
-    pass
+    print('''Training is Completed.
+          The model weights are saved as model_weights.pth.
+          And the plots of the results had been saved as png files.''')
